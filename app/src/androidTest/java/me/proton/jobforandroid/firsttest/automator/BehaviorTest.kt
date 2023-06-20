119909b9261860962caccf7c34c435d4fc67fece
Galyaminsky_Des <jobforandroid@proton.me>
2023-06-20 12:52:20 +0600

    HW_5 Completed

--- app/src/androidTest/java/me/proton/jobforandroid/firsttest/automator/BehaviorTest.kt
+++ app/src/androidTest/java/me/proton/jobforandroid/firsttest/automator/BehaviorTest.kt
@@ -18,90 +18,62 @@ import org.junit.Assert
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
-import me.proton.jobforandroid.firsttest.databinding.ActivityMainBinding
 
 @RunWith(AndroidJUnit4::class)
 @SdkSuppress(minSdkVersion = 18)
 class BehaviorTest {
 
-    //Класс UiDevice предоставляет доступ к вашему устройству.
-    //Именно через UiDevice вы можете управлять устройством, открывать приложения
-    //и находить нужные элементы на экране
     private val uiDevice: UiDevice = UiDevice.getInstance(getInstrumentation())
-
-    //Контекст нам понадобится для запуска нужных экранов и получения packageName
     private val context = ApplicationProvider.getApplicationContext<Context>()
-
-    //Путь к классам нашего приложения, которые мы будем тестировать
     private val packageName = context.packageName
 
     @Before
     fun setup() {
-        //Для начала сворачиваем все приложения, если у нас что-то запущено
         uiDevice.pressHome()
-
-        //Запускаем наше приложение
         val intent = context.packageManager.getLaunchIntentForPackage(packageName)
-        //Мы уже проверяли Интент на null в предыдущем тесте, поэтому допускаем, что Интент у нас не null
-        intent!!.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)//Чистим бэкстек от запущенных ранее Активити
+        intent?.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK) // Чистим бэкстек от запущенных ранее Активити
         context.startActivity(intent)
-
-        //Ждем, когда приложение откроется на смартфоне чтобы начать тестировать его элементы
         uiDevice.wait(Until.hasObject(By.pkg(packageName).depth(0)), TIMEOUT)
     }
 
-    //Убеждаемся, что приложение открыто. Для этого достаточно найти на экране любой элемент
-    //и проверить его на null
     @Test
     fun test_MainActivityIsStarted() {
-        //Через uiDevice находим editText
         val editText = uiDevice.findObject(By.res(packageName, "searchEditText"))
-        //Проверяем на null
-        Assert.assertNotNull(editText)
+        Assert.assertNotNull("EditText is null", editText)
     }
 
-    //Убеждаемся, что поиск работает как ожидается
     @Test
     fun test_SearchIsPositive() {
         val editText = uiDevice.findObject(By.res(packageName, "searchEditText"))
-        Assert.assertNotNull(editText)
+        Assert.assertNotNull("EditText is null", editText)
 
         editText.text = "UiAutomator"
 
         Espresso.onView(ViewMatchers.withId(R.id.searchEditText))
             .perform(ViewActions.pressImeActionButton())
 
-        val changedText = uiDevice.wait(Until.findObject(By.res(packageName, "totalCountTextView")), TIMEOUT)
-        Assert.assertNotNull(changedText)
-        Assert.assertEquals("Number of results: 668", changedText.text)
+        val changedText = uiDevice.wait(
+            Until.findObject(By.res(packageName, "totalCountTextView")),
+            TIMEOUT
+        )
+        Assert.assertNotNull("Changed text is null", changedText)
+        if (changedText != null) {
+            Assert.assertEquals("Number of results: 668", changedText.text)
+        }
     }
 
-    //Убеждаемся, что DetailsScreen открывается
     @Test
     fun test_OpenDetailsScreen() {
-        //Находим кнопку
-        val toDetails: UiObject2 = uiDevice.findObject(
-            By.res(
-                packageName,
-                "toDetailsActivityButton"
-            )
-        )
-        //Кликаем по ней
-        toDetails.click()
+        val toDetails: UiObject2? = uiDevice.findObject(By.res(packageName, "toDetailsActivityButton"))
+        Assert.assertNotNull("toDetails button is null", toDetails)
 
-        //Ожидаем конкретного события: появления текстового поля totalCountTextView.
-        //Это будет означать, что DetailsScreen открылся и это поле видно на экране.
-        val changedText =
-            uiDevice.wait(
-                Until.findObject(By.res(packageName, "totalCountTextView")),
-                TIMEOUT
-            )
-        //Убеждаемся, что поле видно и содержит предполагаемый текст.
-        //Обратите внимание, что текст должен быть "Number of results: 0",
-        //так как мы кликаем по кнопке не отправляя никаких поисковых запросов.
-        //Чтобы проверить отображение определенного количества репозиториев,
-        //вам в одном и том же методе нужно отправить запрос на сервер и открыть DetailsScreen.
-        Assert.assertEquals(changedText.text, "Number of results: 0")
+        toDetails?.click()
+
+        val changedText = uiDevice.wait(
+            Until.findObject(By.res(packageName, "totalCountTextView")),
+            TIMEOUT
+        )
+        Assert.assertEquals("Number of results: 0", changedText?.text)
     }
 
     companion object {
--- app/src/androidTest/java/me/proton/jobforandroid/firsttest/espresso/MainActivityEspressoTest.kt
+++ app/src/androidTest/java/me/proton/jobforandroid/firsttest/espresso/MainActivityEspressoTest.kt
@@ -1,5 +1,7 @@
 package me.proton.jobforandroid.firsttest.espresso
 
+import android.content.Context
+import android.provider.Settings.System.getString
 import android.view.View
 import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso.onView
@@ -9,6 +11,7 @@ import androidx.test.espresso.action.ViewActions.*
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
 import me.proton.jobforandroid.firsttest.BuildConfig
 import me.proton.jobforandroid.firsttest.R
 import me.proton.jobforandroid.firsttest.view.search.MainActivity
@@ -17,6 +20,8 @@ import org.junit.After
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.junit.runner.manipulation.Ordering
+import java.util.Locale
 
 @RunWith(AndroidJUnit4::class)
 class MainActivityEspressoTest {
@@ -38,11 +43,12 @@ class MainActivityEspressoTest {
             onView(withId(R.id.totalCountTextView)).check(matches(withText("Number of results: 42")))
         } else {
             onView(isRoot()).perform(delay())
-            onView(withId(R.id.totalCountTextView)).check(matches(withText("Number of results: 2283")))
+            val expectedText = String.format(Locale.getDefault(), "Number of results: %d", 2283)
+            onView(withId(R.id.totalCountTextView)).check(matches(withText(expectedText)))
         }
     }
 
-    private fun delay(): ViewAction? {
+    private fun delay(): ViewAction {
         return object : ViewAction {
             override fun getConstraints(): Matcher<View> = isRoot()
             override fun getDescription(): String = "wait for $2 seconds"
