f60946ad7c7c330dab81c10f5446f5a8dc0137c1
Galyaminsky_Des <jobforandroid@proton.me>
2023-06-20 12:56:11 +0600

    HW_5 Completed Review

--- app/src/androidTest/java/me/proton/jobforandroid/firsttest/espresso/MainActivityEspressoTest.kt
+++ app/src/androidTest/java/me/proton/jobforandroid/firsttest/espresso/MainActivityEspressoTest.kt
@@ -1,7 +1,5 @@
 package me.proton.jobforandroid.firsttest.espresso
 
-import android.content.Context
-import android.provider.Settings.System.getString
 import android.view.View
 import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso.onView
@@ -11,7 +9,6 @@ import androidx.test.espresso.action.ViewActions.*
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import androidx.test.platform.app.InstrumentationRegistry
 import me.proton.jobforandroid.firsttest.BuildConfig
 import me.proton.jobforandroid.firsttest.R
 import me.proton.jobforandroid.firsttest.view.search.MainActivity
@@ -20,14 +17,12 @@ import org.junit.After
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.junit.runner.manipulation.Ordering
 import java.util.Locale
 
 @RunWith(AndroidJUnit4::class)
 class MainActivityEspressoTest {
 
     private lateinit var scenario: ActivityScenario<MainActivity>
-
     @Before
     fun setup() {
         scenario = ActivityScenario.launch(MainActivity::class.java)
